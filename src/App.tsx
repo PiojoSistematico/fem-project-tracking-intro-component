d29bfefef521e0176ad933519797bbf30ff20bdd
Jorge Saa <piojo.sistematico@gmail.com>
2023-08-31 20:45:16 -0400

    added mobile css

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,29 @@
 import logo from "./assets/images/logo.svg";
 import illustration from "./assets/images/illustration-devices.svg";
+import Menu from "./components/Menu";
 
 function App() {
   return (
-    <main>
-      <header>
-        <picture>
-          <img src={logo} alt="" />
-        </picture>
-        <nav>
-          <ul>
-            <li>
-              <a href="">Product</a>
-            </li>
-            <li>
-              <a href="">Features</a>
-            </li>
-            <li>
-              <a href="">Pricing</a>
-            </li>
-
-            <li>
-              <a href="">Login</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <section className="main-section">
+    <>
+      <Menu></Menu>
+      <main>
         <picture>
           <img src={illustration} alt="" />
         </picture>
-        <div>
-          <div>
-            <span>New</span>
-            <span>Monograph Dashboard</span>
+        <section className="main-section">
+          <div className="flex-row">
+            <span className="black-span">New</span>
+            <span className="uppercase">Monograph Dashboard</span>
           </div>
           <h1>Powerful insights into your team</h1>
           <p>Project planning and time tracking for agile teams</p>
-          <div>
-            <button>Schedule a demo</button>
-            <span>to see a live preview</span>
+          <div className="flex-row">
+            <button className="accent">Schedule a demo</button>
+            <span className="uppercase">to see a live preview</span>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </>
   );
 }
 
